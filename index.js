b5ad9f54d2491e0b5dc50e596d8dc29aa6f6bbf0
Ron Forbes <ronforbes528@gmail.com>
2017-07-18 20:44:54 -0700

    Added .gitignore and node.js server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io').listen(server)
+
+app.use('/',express.static(__dirname + '/bin'));
+app.use('/js',express.static(__dirname + '/bin/js'));
+app.use('/assets',express.static(__dirname + '/bin/assets'));
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/bin/index.html');
+});
+
+//server.listen(process.env.PORT || 8081,function(){
+//    console.log('Listening on '+server.address().port);
+//});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
